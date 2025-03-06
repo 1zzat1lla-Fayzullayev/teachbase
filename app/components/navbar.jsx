985d717967c65cfc7f22ce498b08748a9d386fc6
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-03-06 17:49:28 +0500

    kop narsa qoshilmadi ulgrmadm

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,9 +1,10 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Wrapper from "../layout/wrapper";
 
 function Navbar() {
   const [value, setValue] = React.useState("");
+  const [mobile, setMobile] = React.useState("")
 
   const refInput = React.useRef(null);
 
@@ -41,13 +42,17 @@ function Navbar() {
     };
   }, []);
 
+  const handleOpenMobileMenu = () => {
+    setMobile(!mobile)
+  }
+
   return (
     <>
       <div className="bg-transparent h-[65px]">
         <Wrapper>
-          <div className="px-6 fixed w-full left-0 top-0 py-[15px] border-b border-b-[#9ca3af33] flex justify-between items-center">
+          <div className="px-6 fixed w-full left-0 top-0 py-[15px] border-b border-b-[#9ca3af33] flex justify-between items-center z-[999]" style={{backdropFilter: "blur(5px)"}}>
             <img src="/logo.svg" className="cursor-pointer" />
-            <ul className="flex items-center gap-5">
+            <ul className="items-center gap-5 hidden md:flex">
               <li>
                 <a
                   href="#"
@@ -87,7 +92,7 @@ function Navbar() {
                   value={value}
                   onChange={(e) => setValue(e.target.value)}
                   placeholder="Найти..."
-                  className="px-3 py-[7px] transition-all text-sm border-none w-[256px] rounded-lg bg-[#f9fafb1a] focus:bg-[#111111]  text-gray-500 placeholder:text-gray-400 dark:text-gray-300  border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="px-3 py-[7px] transition-all text-sm border-none lg:w-[256px] rounded-lg bg-[#f9fafb1a] focus:bg-[#111111]  text-gray-500 placeholder:text-gray-400 dark:text-gray-300  border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 <kbd className="absolute top-0 my-1.5 select-none ltr:right-1.5 rtl:left-1.5 h-5 rounded bg-white px-1.5 font-mono text-[10px] font-medium text-gray-500 border dark:border-gray-100/20 dark:bg-[#111111]/50 contrast-more:border-current contrast-more:text-current contrast-more:dark:border-current items-center gap-1 pointer-events-none hidden sm:flex opacity-100">
                   {
--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,11 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark" style={{ scrollBehavior: "smooth", colorScheme: "dark" }}>
+    <html
+      lang="en"
+      className="dark"
+      style={{ scrollBehavior: "smooth", colorScheme: "dark" }}
+    >
       <body className={`antialiased`}>{children}</body>
     </html>
   );
