87f7eb158d2c90c080b6202788104ca841a6c39f
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-03-09 12:01:59 +0500

    change

--- app/components/managers.jsx
+++ app/components/managers.jsx
@@ -21,8 +21,84 @@ function Managers() {
                 </Link>
               </h4>
             </div>
-            <div className="content flex flex-row flex-wrap gap-4 mt-2">
-              <div className="carde max-w-[370px] border rounded-2xl flex flex-col dark:text-gray-100 bg-white dark:bg-[#111] drop-shadow-lg p-5 border-gray-200">
+            <div className="content grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-2">
+              <div className="carde sm:w-full lg:max-w-[370px] border rounded-2xl flex flex-col dark:text-gray-100 bg-white dark:bg-[#111] drop-shadow-lg p-5 border-gray-200">
+                <h5 className="text-xl font-light mb-5">Продукт 01</h5>
+                <p className="text-sm leading-normal mb-5">
+                  Одна из базовых функций платформы - возможность создавать и
+                  запускать обучение по курсам. Которые могут состоять
+                  практически из любого вида контента: аудио, видео, документы,
+                  опросы, SCORM, html-страницы, встроенные материалы из
+                  сторонних сервисов: Youtube, Vimeo-видео, Google Forms, Google
+                  Docs, LearningApps, H5P и многое-многое другое. А для оценки
+                  знаний использовать встроенные инструменты - конструктор
+                  тестов и заданий.
+                </p>
+                <Link
+                  className="uppercase mt-auto text-blue-400 font-bold hover:opacity-70"
+                  href="#"
+                >
+                  22 статьи
+                </Link>
+              </div>
+              <div className="carde sm:w-full lg:max-w-[370px] border rounded-2xl flex flex-col dark:text-gray-100 bg-white dark:bg-[#111] drop-shadow-lg p-5 border-gray-200">
+                <h5 className="text-xl font-light mb-5">Продукт 01</h5>
+                <p className="text-sm leading-normal mb-5">
+                  Одна из базовых функций платформы - возможность создавать и
+                  запускать обучение по курсам. Которые могут состоять
+                  практически из любого вида контента: аудио, видео, документы,
+                  опросы, SCORM, html-страницы, встроенные материалы из
+                  сторонних сервисов: Youtube, Vimeo-видео, Google Forms, Google
+                  Docs, LearningApps, H5P и многое-многое другое. А для оценки
+                  знаний использовать встроенные инструменты - конструктор
+                  тестов и заданий.
+                </p>
+                <Link
+                  className="uppercase mt-auto text-blue-400 font-bold hover:opacity-70"
+                  href="#"
+                >
+                  22 статьи
+                </Link>
+              </div>
+              <div className="carde sm:w-full lg:max-w-[370px] border rounded-2xl flex flex-col dark:text-gray-100 bg-white dark:bg-[#111] drop-shadow-lg p-5 border-gray-200">
+                <h5 className="text-xl font-light mb-5">Продукт 01</h5>
+                <p className="text-sm leading-normal mb-5">
+                  Одна из базовых функций платформы - возможность создавать и
+                  запускать обучение по курсам. Которые могут состоять
+                  практически из любого вида контента: аудио, видео, документы,
+                  опросы, SCORM, html-страницы, встроенные материалы из
+                  сторонних сервисов: Youtube, Vimeo-видео, Google Forms, Google
+                  Docs, LearningApps, H5P и многое-многое другое. А для оценки
+                  знаний использовать встроенные инструменты - конструктор
+                  тестов и заданий.
+                </p>
+                <Link
+                  className="uppercase mt-auto text-blue-400 font-bold hover:opacity-70"
+                  href="#"
+                >
+                  22 статьи
+                </Link>
+              </div>
+              <div className="carde sm:w-full lg:max-w-[370px] border rounded-2xl flex flex-col dark:text-gray-100 bg-white dark:bg-[#111] drop-shadow-lg p-5 border-gray-200">
+                <h5 className="text-xl font-light mb-5">Продукт 01</h5>
+                <p className="text-sm leading-normal mb-5">
+                  Одна из базовых функций платформы - возможность создавать и
+                  запускать обучение по курсам. Которые могут состоять
+                  практически из любого вида контента: аудио, видео, документы,
+                  опросы, SCORM, html-страницы, встроенные материалы из
+                  сторонних сервисов: Youtube, Vimeo-видео, Google Forms, Google
+                  Docs, LearningApps, H5P и многое-многое другое. А для оценки
+                  знаний использовать встроенные инструменты - конструктор
+                  тестов и заданий.
+                </p>
+                <Link
+                  className="uppercase mt-auto text-blue-400 font-bold hover:opacity-70"
+                  href="#"
+                >
+                  22 статьи
+                </Link>
+              </div>
+              <div className="carde sm:w-full lg:max-w-[370px] border rounded-2xl flex flex-col dark:text-gray-100 bg-white dark:bg-[#111] drop-shadow-lg p-5 border-gray-200">
                 <h5 className="text-xl font-light mb-5">Продукт 01</h5>
                 <p className="text-sm leading-normal mb-5">
                   Одна из базовых функций платформы - возможность создавать и
